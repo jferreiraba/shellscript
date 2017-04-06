a7058aff965392d5ed2c6f017a97b7c1f91f7cf0
JohnJohn <jferreira.ba@gmail.com>
2017-04-06 02:27:45 -0300

    Update net.sh

--- net.sh
+++ net.sh
@@ -6,27 +6,41 @@
 # Description: Shellscript facilita a configuração do WIFI AD-HOC no linux.
 #
 
+#
+# Variaveis globais.
+#
+INTERFACE_WLAN=""
+SSID=""
+CHANNEL=""
+PASSWORD=""
+IPTABLES="/sbin/iptables"
+IP=""
+MASK=""
+
+#
+# Não modifique nenhuma linha abaixo.
+#
 echo -n "Ajustando o firewall..."
-iptables -t nat -F
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+$IPTABLES -t nat -F
+$IPTABLES -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo -e "\033[0;32m OK \033[0m "
 
 echo -n "Desligando a interface wlan0... "
-ifconfig wlan0 down
+ifconfig $INTERFACE_WLAN down
 echo -e "\033[0;32m OK \033[0m "
 
 echo -n "Configurando ad-hoc... "
-iwconfig wlan0 essid h2k mode Ad-Hoc channel 11 key restricted 53445678AF
+iwconfig $INTERFACE_WLAN essid $SSID mode Ad-Hoc channel $CHANNEL key restricted $PASSWORD
 echo -e "\033[0;32m OK \033[0m "
 
 echo -n "Subindo a interface wlan0... "
-ifconfig wlan0 192.168.0.1 netmask 255.255.255.0 up
+ifconfig $INTERFACE_WLAN $IP netmask $MASK up
 echo -e "\033[0;32m OK \033[0m "
 
 echo -n "Verificando o servidor DHCP... "
 killall dhcpd 2> /dev/null
-dhcpd wlan0 2> /dev/null
+dhcpd $INTERFACE_WLAN 2> /dev/null
 echo -e "\033[0;32m OK \033[0m "
 echo "Rede ad-hoc configurada..."
 
